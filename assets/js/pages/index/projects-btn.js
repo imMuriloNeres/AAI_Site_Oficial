25c97d7f9def5fb633d6efe4ee576f421096ffc9
Murilo <muriloneuba@gmail.com>
2024-08-04 16:23:40 -0300

    NO JS COMMENTS

--- assets/js/pages/index/projects-btn.js
+++ assets/js/pages/index/projects-btn.js
@@ -3,45 +3,39 @@ document.addEventListener('DOMContentLoaded', () => {
   const showMoreButton = document.getElementById('show-more');
   const showLessButton = document.getElementById('show-less');
 
-  let currentVisibleCards = 4; // Número inicial de cards visíveis
+  let currentVisibleCards = 4;
 
-  // Função para mostrar apenas os cards visíveis
   const showCards = (limit) => {
       cards.forEach((card, index) => {
           if (index < limit) {
-              card.style.display = 'flex'; // Mostra o card
+              card.style.display = 'flex';
           } else {
-              card.style.display = 'none'; // Esconde o card
+              card.style.display = 'none';
           }
       });
 
-      // Atualiza a visibilidade dos botões
       updateButtons(limit);
   };
 
-  // Função para atualizar a visibilidade dos botões
   const updateButtons = (visibleCount) => {
       if (visibleCount >= cards.length) {
-          showMoreButton.style.display = 'none'; // Todos os cards foram mostrados
-          showLessButton.style.display = 'inline-block'; // Mostra o botão de fechar
+          showMoreButton.style.display = 'none';
+          showLessButton.style.display = 'inline-block';
       } else {
-          showMoreButton.style.display = 'inline-block'; // Ainda há mais cards a mostrar
-          showLessButton.style.display = 'none'; // Oculta o botão de fechar
+          showMoreButton.style.display = 'inline-block';
+          showLessButton.style.display = 'none';
       }
   };
 
-  // Mostra os primeiros 4 cards inicialmente
   showCards(currentVisibleCards);
 
-  // Evento de clique para o botão "Mais Oficinas e Cursos"
   showMoreButton.addEventListener('click', () => {
-      currentVisibleCards += 4; // Aumenta o número de cards visíveis em 4
-      showCards(currentVisibleCards); // Atualiza a exibição dos cards
+      currentVisibleCards += 4;
+      showCards(currentVisibleCards);
   });
 
-  // Evento de clique para o botão "Fechar"
   showLessButton.addEventListener('click', () => {
-      currentVisibleCards = 4; // Redefine para mostrar apenas os 4 primeiros
-      showCards(currentVisibleCards); // Atualiza a exibição dos cards
+      currentVisibleCards = 4;
+      showCards(currentVisibleCards);
   });
 });
